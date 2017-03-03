e1bc66bbeb53a871cd59ae683534c40e57a8a9d1
Bohdan <bohdan.zhyvko@gmail.com>
2017-03-03 10:18:37 +0000

    change title image

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 //contact information hiden in html
 var contactEmail = document.getElementById('contact-email-write');
-contactEmail.innerHTML = ['com','.','gmail','@','zhyvko','.',
-                            'bohdan'].reverse().join('');
+var arrEmail = ['com','.','gmail','@','zhyvko','.','bohdan'];
+contactEmail.innerHTML = arrEmail.reverse().join('');
+
 var contactPhoneNumber = document.getElementById('contact-phone-write');
 contactPhoneNumber.innerHTML = ['9','3','4','2','-',,'8','5','1','-','3','6','0',
                             '-','8','3','+'].reverse().join('');
 var contactSkype = document.getElementById('contact-skype-write');
 contactSkype.innerHTML = ['zhyvko','.','bohdan'].reverse().join('');
 
-//google Map Lviv
+//send form contact me on email
+function doSubmit(){
+    var formAction = document.getElementById('form-send-massege')
+    formAction.action = 'https://formspree.io/' + arrEmail.join('');
+    formAction.submit();
+}
+
+//google Map
 function myMap() {
     var mapProp= {
         center:new google.maps.LatLng(49.8432204,24.0259262),
