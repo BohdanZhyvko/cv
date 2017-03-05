45a1a5e5c52d95612c14eaab1a25d35ae1e4f57d
Bohdan <bohdan.zhyvko@gmail.com>
2017-03-05 11:13:38 +0000

    chenged form submit, and create function sendMasseg in js

--- js/index.js
+++ js/index.js
@@ -9,12 +9,16 @@ contactPhoneNumber.innerHTML = ['9','3','4','2','-',,'8','5','1','-','3','6','0'
 var contactSkype = document.getElementById('contact-skype-write');
 contactSkype.innerHTML = ['zhyvko','.','bohdan'].reverse().join('');
 
+
 //send form contact me on email
-function doSubmit(){
-    var formAction = document.getElementById('form-send-massege')
+var formAction = document.getElementById('form-send-massege');
+formAction.addEventListener('submit', function() {sendMassege()} );
+
+function sendMassege(){
     formAction.action = 'https://formspree.io/' + arrEmail.join('');
-    formAction.submit();
 }
+
+
 
 //google Map
 function myMap() {
