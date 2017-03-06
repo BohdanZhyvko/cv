bfd03aae68767ab722d80f72484f64df5ffedbe9
Bohdan <bohdan.zhyvko@gmail.com>
2017-03-06 13:33:58 +0000

    fixed nav bar

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
 //contact information hiden in html
 var contactEmail = document.getElementById('contact-email-write');
-var arrEmail = ['com','.','gmail','@','zhyvko','.','bohdan'];
+
+var arrEmail = ['com', '.', 'gmail', '@', 'zhyvko', '.', 'bohdan'];
 contactEmail.innerHTML = arrEmail.reverse().join('');
 
 var contactPhoneNumber = document.getElementById('contact-phone-write');
-contactPhoneNumber.innerHTML = ['9','3','4','2','-',,'8','5','1','-','3','6','0',
-                            '-','8','3','+'].reverse().join('');
-var contactSkype = document.getElementById('contact-skype-write');
-contactSkype.innerHTML = ['zhyvko','.','bohdan'].reverse().join('');
+contactPhoneNumber.innerHTML = ['9', '3', '4', '2', '-', , '8', '5', '1', '-', '3', '6', '0', '-', '8', '3', '+'].reverse().join('');
 
+var contactSkype = document.getElementById('contact-skype-write');
+contactSkype.innerHTML = ['zhyvko', '.', 'bohdan'].reverse().join('');
 
 //send form contact me on email
 var formAction = document.getElementById('form-send-massege');
-formAction.addEventListener('submit', function() {sendMassege()} );
+formAction.addEventListener('submit', function() {
+    sendMassege()
+});
 
-function sendMassege(){
+function sendMassege() {
     formAction.action = 'https://formspree.io/' + arrEmail.join('');
 }
 
 //google Map
 function myMap() {
-    var mapProp= {
-        center:new google.maps.LatLng(49.8432204,24.0259262),
-        zoom:17,
+    var mapProp = {
+        center: new google.maps.LatLng(49.8432204,24.0259262),
+        zoom: 17,
     };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
