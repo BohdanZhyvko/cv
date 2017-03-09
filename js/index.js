e27b34d00b189a42bafc1672622b80f02701fac5
Bohdan <bohdan.zhyvko@gmail.com>
2017-03-09 15:50:24 +0000

    optimized for mobile home page

--- js/index.js
+++ js/index.js
@@ -19,12 +19,3 @@ formAction.addEventListener('submit', function() {
 function sendMassege() {
     formAction.action = 'https://formspree.io/' + arrEmail.join('');
 }
-
-//google Map
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(49.8432204,24.0259262),
-        zoom: 17,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
